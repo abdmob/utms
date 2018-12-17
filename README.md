b1f4e61b81e53d89f97a3e154406d9ad59c14621
Abdulla Abdurakhmanov <abdulla.abd.m@gmail.com>
2018-12-17 21:35:20 +0300

    Readme updates

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# utms
+Implementation of Universal Turing Machine in Scala that can simulate an arbitrary Turing machine on arbitrary input
+
+Details here: https://rosettacode.org/wiki/Universal_Turing_machine
--- src/main/scala/utm/scala/UniversalTuringMachine.scala
+++ src/main/scala/utm/scala/UniversalTuringMachine.scala
@@ -23,6 +23,7 @@ import scala.reflect.classTag
 /**
   * Implementation of Universal Turing Machine in Scala that can simulate an arbitrary
   * Turing machine on arbitrary input
+  * @author Abdulla Abdurakhmanov (https://github.com/abdmob/utms)
   */
 class UniversalTuringMachine[S](val rules: List[UTMRule[S]],
                                 val initialState: S,
